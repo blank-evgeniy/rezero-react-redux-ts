852a2c7aa4b9fa8fd4644ba4cc6df78974152a64
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-06-16 22:51:21 +0200

    The home page is stylized

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,13 +6,26 @@ const Home = () => {
 
     return (
         <main className="home">
-            <h1 className="home__title">
-                Re zero
-            </h1>
-            <p className="home__info">
-                charecters_database
-            </p>
-            <button onClick={ () => navigate("/characters")}>TO THE WEBSITE</button>
+            <section className="home__logo-section">
+                <img
+                    className="home__logo" 
+                    src="/images/home/home_logo.png"
+                    alt="re: zero"
+                />
+                <h1 className="home__title">characters_database</h1>
+            </section>
+
+            <section 
+                className="home__nav-section" 
+                style={{backgroundImage: "url(/images/home/home_back.jpg)"}}
+            >
+                <button 
+                    className="home__button" 
+                    onClick={ () => navigate("/characters")}
+                >
+                    TO THE <br/> WEBSITE
+                </button>
+            </section>
         </main>
     );
 };
